2d5466f9e83ad73e6cba2c302ff689e576db7eae
Corina Jewer <corina.jewer@keyin.com>
2024-02-29 19:00:11 -0330

    Added INSERT for Invoice_Details

---
+++ INSERT/Invoice_Details_mock_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES ( null, 1, null, 1);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (1, null, 2, 2);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (1, null, 1, 2);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null, 3, null, 3);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null, 7, null, 3);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null, 8, null, 4);	
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null, 6, null, 5);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null, 8, null, 6);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null , 3, null, 7);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null , 4, null, 8);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (6, null, 1, 9);
+INSERT INTO public."Invoice_Details"(product_id, service_id, product_quantity, invoice_id)
+	VALUES (null, 6, null, 10);
